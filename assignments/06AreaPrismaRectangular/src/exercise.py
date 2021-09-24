0b4be360fa0a5800b8f3a2696e7631cfd78a0588
MauricioRodriguez02 <A00835048@tec.mx>
2021-09-24 00:42:51 -0500

    SUPER CORREGIDO

--- assignments/06AreaPrismaRectangular/src/exercise.py
+++ assignments/06AreaPrismaRectangular/src/exercise.py
@@ -5,12 +5,12 @@ def area_prisma(base,altura,profundidad):
     return area(base,altura)*2+area(altura,profundidad)*2+area(base,profundidad)*2
 
 def main():
-    #escribe tu código abajo de esta línea
+    
     b = float(input("Dame la base: "))
     a = float(input("Dame la altura: "))
     p = float(input("Dame la profundidad: "))
 
-    r = area_prisma(b,a,p)
+    r = (b*a)*2+(a*p)*2+(b*p)*2
 
     print("El área total del prisma es:",r)
 
